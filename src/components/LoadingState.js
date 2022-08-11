297be890d3a701d797cc7df2694cef1a9799e0a2
chenaoh <cdhena@gmail.com>
2022-08-11 13:13:15 -0500

    upload files

---
+++ src/components/LoadingState.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LoadingState = () => {
+  return (
+    <div className='spinner'>
+      <article></article>
+      <p className='text-white opacity-75 mt-5'>
+        The spinny thingy means it's loading
+      </p>
+    </div>
+  )
+}
+
+export default LoadingState
